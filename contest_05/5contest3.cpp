ac936744444b8b3a1e87fe78a6dd383df848cb43
kenctxncy <144899925+kenctxncy@users.noreply.github.com>
2023-11-08 23:02:56 +0300

    uploaded contest#5 files (only number 5 left)

---
+++ contest_05/5contest3.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+
+#include <sstream>
+#include <cmath>
+class Complex {
+public:
+    double re, im;
+
+    Complex(const std::string& str) {
+        std::stringstream iss(str);
+        char op = '+';
+        iss >> re;
+        iss >> std::ws;
+        if (iss.peek() == '+' || iss.peek() == '-') {
+            iss >> op;
+        }
+        iss >> std::ws;
+        iss >> im;
+        if (op == '-') {
+            im = -im;
+        }
+    }
+
+    Complex operator+(const Complex& num2) const {
+        return Complex(re + num2.re, im + num2.im);
+    }
+
+    Complex operator-(const Complex& num2) const {
+        return Complex(re - num2.re, im - num2.im);
+    }
+
+    Complex operator*(const Complex& num2) const {
+        return Complex(re * num2.re - im * num2.im, re * num2.im + im * num2.re);
+    }
+
+    Complex operator/(const Complex& num2) const {
+        double del = pow(num2.re, 2) + pow(num2.im, 2);
+        double real = (re * num2.re + im * num2.im) / del;
+        double imag = (im * num2.re - re * num2.im) / del;
+        return Complex(real,imag);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Complex& num) {
+        out << (num.re == 0.0 ? 0.0 : num.re)
+            << (num.im < 0 ? "" : "+") << (num.im == 0.0 ? 0.0 : num.im) << 'j';
+        return out;
+    }
+
+private:
+    Complex(double re, double im) : re(re), im(im) {}
+};
+
+int main()
+{
+    std::string num;
+    std::getline(std::cin, num, 'j');
+    Complex c1(num);
+
+    std::getline(std::cin, num, 'j');
+    Complex c2(num);
+
+    std::cout << c1 + c2 << '\n' << c1 - c2 << '\n'
+        << c1 * c2 << '\n' << c1 / c2 << std::endl;
+}
---
+++ contest_05/5contest4.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include "json.hpp"
+using json = nlohmann::json;
+
+#include <cmath>
+class Point {
+public:
+    Point(double x, double y) : x(x), y(y) {}
+    double get_x() const {
+        return x;
+    }
+    double get_y() const {
+        return y;
+    }
+    double get_r() const {
+        return std::sqrt(pow(x, 2) + pow(y, 2));
+    }
+    double get_a() const {
+        return std::atan2(y, x);
+    }
+
+    void set_x(double x) {
+        this->x = x;
+    }
+    void set_y(double y) {
+        this->y = y;
+    }
+    void set_r(double r) {
+        double a = get_a();
+        x = r * std::cos(a);
+        y = r * std::sin(a);
+    }
+    void set_a(double a) {
+        double r = get_r();
+        x = r * std::cos(a);
+        y = r * std::sin(a);
+    }
+private:
+    double x, y;
+};
+
+int main() {
+    double x, y;
+    std::cin >> x >> y;
+
+    Point p(x, y);
+    json moves;
+    std::cin >> moves;
+
+    for (auto& move : moves) {
+        std::string ort = move["ort"];
+        double value = move["value"];
+        if (ort == "x") p.set_x(p.get_x() + value);
+        else if (ort == "y") p.set_y(p.get_y() + value);
+        else if (ort == "r") p.set_r(p.get_r() + value);
+        else p.set_a(p.get_a() + value);
+        std::cout << round(p.get_x() * (1E+5)) / (1E+5) << ' ' << round(p.get_y() * (1E+5)) / (1E+5) << std::endl;
+    }
+}
