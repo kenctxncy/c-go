2c94cf7c646d78622cd467172eb5d69a25a4711a
kenctxncy <144899925+kenctxncy@users.noreply.github.com>
2023-12-06 03:24:22 +0300

    uploaded contest6 files
    2 hour speedrun any% terribly failed. missed such a stupid thing in 2nd task)0)

---
+++ contest_06/6contest1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+#include <cmath>
+
+class Shape {
+public:
+    virtual double getPerimeter() = 0;
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(int a, int b) : a(a), b(b) {}
+    double getPerimeter() override {
+        return (a + b) * 2;
+    }
+private:
+    int a, b;
+};
+
+class Circle : public Shape {
+public:
+    Circle(int r) : r(r) {}
+    double getPerimeter() override {
+        return r * PI * 2;
+    }
+private:
+    int r;
+    const long double PI = std::acos(-1.0);
+};
+
+class Triangle : public Shape {
+public:
+    Triangle(int a, int b, int c) : a(a), b(b), c(c) {}
+    double getPerimeter() override{
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return 0;
+        }
+        return a + b + c;
+    }
+private:
+    int a, b, c;
+};
+
+int main() {
+    int momentum;
+    std::cin >> momentum;
+    srand(momentum);
+    std::vector <Shape*> shapes(rand() % 100 + 1);
+    for (int i = 0; i < shapes.size(); i++) {
+        int a = rand() % 10 + 1, b = rand() % 10 + 1, c = rand() % 10 + 1;
+        if (rand() % 3 == 0) shapes[i] = new Rectangle(a, b);
+        else if (rand() % 3 == 1) shapes[i] = new Circle(c);
+        else shapes[i] = new Triangle(a, b, c);
+    }
+
+    double sum = 0;
+    for (auto shape : shapes) { sum += shape->getPerimeter(); }
+    std::cout << (int)sum;
+}
---
+++ contest_06/6contest2.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+/// <summary>
+/// dlya spravki ya do six por ne ponyal pochemu u menya vivod vo vtorom primere vsegda toi zhe zarplatu chto i po usloviyu
+/// </summary>
+std::map<std::string, double> bonuses;
+//sss
+#include <cmath>
+
+class Employee {
+public:
+    Employee(const std::string& name, const std::string& position, int salary) : name(name), position(position), salary(salary) {}
+
+    virtual int bonus(double percent) {
+        return round(salary * percent);
+    }
+
+    int total_cash(double bonusPercent) {
+        return salary + bonus(bonusPercent);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, Employee& dude) {
+        out << dude.name << " (" << dude.position << "): " << dude.total_cash(bonuses[dude.position]);
+        return out;
+    }
+
+protected:
+    std::string name, position;
+    int salary;
+};
+
+class Manager : public Employee {
+public:
+    Manager(const std::string& name, int money = 16242)
+        :Employee(name, "manager", money) {
+    }
+
+    int bonus(double percent) override {
+        double actualPercent = (percent < 0.1) ? 0.1 : percent;
+        return round(salary * actualPercent);
+    }
+};
+
+//sss
+class Boss : public Employee {
+public:
+    Boss(std::string name, int money = 16242)
+        :Employee(name, "boss", money) {
+    }
+};
+
+int main() {
+    double boss_bonus, manager_bonus;
+    std::cin >> boss_bonus >> manager_bonus;
+    bonuses["manager"] = manager_bonus;
+    bonuses["boss"] = boss_bonus;
+
+    int john_money, bob_money, alice_money;
+    std::cin >> john_money >> bob_money >> alice_money;
+    Boss john("John", john_money);
+    Manager bob("Bob", bob_money);
+    Manager alice("Alice", alice_money);
+
+    std::cout << john << '\n' << bob << '\n' << alice << std::endl;
+}
